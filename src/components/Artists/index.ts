3f37d7f4e0574c5620a6979ca809709611650135
vilart1vilart1 <vilart1@respizenmedical.com>
2025-04-13 16:27:43 +0100

    oki

--- src/components/Artists/index.ts
+++
@@ -1,8 +0,0 @@
-
-export { default as ArtistInfo } from './ArtistInfo';
-export { default as ArtistStats } from './ArtistStats';
-export { default as ArtistNavTabs } from './ArtistNavTabs';
-export { default as Overview } from './Overview';
-export { default as ProjectsList } from './ProjectsList';
-export { default as DocumentsList } from './DocumentsList';
-export * from './modals';
--- src/components/Artists/modals.ts
+++
@@ -1,5 +0,0 @@
-
-export { default as ProjectModal } from './Modals/ProjectModal';
-export { default as TaskModal } from './Modals/TaskModal';
-export { default as FileUploadModal } from './Modals/FileUploadModal';
-export { default as ArtistModal } from './Modals/ArtistModal';
