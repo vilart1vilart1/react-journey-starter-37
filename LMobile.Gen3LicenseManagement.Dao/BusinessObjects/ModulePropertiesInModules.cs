1a9bfaa6e48fb846e0be18275c3aad8e91fdd543
vilart1vilart1 <vilart1@respizenmedical.com>
2025-05-23 09:20:20 +0100

    ed

--- LMobile.Gen3LicenseManagement.Dao/BusinessObjects/ModulePropertiesInModules.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace LMobile.Gen3LicenseManagement.Dao.BusinessObjects {
-	public class ModulePropertiesInModules : StoredModulePropertiesInModules {
-	}
-}
--- LMobile.Gen3LicenseManagement.Portal/Applications/BaseApplication.cs
+++
@@ -1,8 +0,0 @@
-﻿using LMobile.Gida;
-using LMobile.MiniForms;
-
-namespace LMobile.Gen3LicenseManagement.Portal.Applications {
-	class BaseApplication : MiniFormsApplication {
-		protected GidaSession Session { get { return GidaConfiguration.Factories["LicenseManagement"].CurrentSession; } }
-	}
-}
